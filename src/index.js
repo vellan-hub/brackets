953a14d06eb3d71cb5fbd88fcce2c6e7a91117e6
Vitaly Yakubovski <falkon777.13@gmail.com>
2021-03-15 15:43:53 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairs = bracketsConfig;
+  let open = new Set(pairs.map(pair => pair[0]));
+  let close  = new Set(pairs.map(pair => pair[1]));
+  let relevant = pairs.reduce((acc, [open, close]) => ({...acc, [close]: open}), {});
+
+  let stack = [];
+  for (let char of str) {
+    if (open.has(char)) {
+      stack.push(char);
+    }
+    if (close.has(char)) {
+      if (relevant[char] !== stack.pop()) return false;
+    }
+  }
+
+  return stack.length == 0;
 }
